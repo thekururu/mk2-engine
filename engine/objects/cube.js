8bdc11afd607c31267a49cdef37d16e508706815
MyThekuruku <thekuruky@gmail.com>
2026-01-25 17:56:31 -0600

    Update cube.js

--- engine/objects/cube.js
+++ engine/objects/cube.js
@@ -1,18 +1,25 @@
+// engine/objects/cube.js
+
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 import { scene } from "../scene/scene.js";
 
 export function createCube() {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({ color: 0x00ffcc });
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x00ffcc,
+  });
+
   const cube = new THREE.Mesh(geometry, material);
 
   cube.position.set(
-    Math.random() * 2 - 1,
-    Math.random() * 2 - 1,
+    Math.random() * 4 - 2,
+    Math.random() * 4 - 2,
     0
   );
 
   scene.add(cube);
+
   console.log("🧊 Cubo creado");
   return cube;
 }
+
