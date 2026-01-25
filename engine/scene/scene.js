1662365f220ac615a9be32c6c7427083190bcab2
MyThekuruku <thekuruky@gmail.com>
2026-01-25 17:32:27 -0600

    Update scene.js

--- engine/scene/scene.js
+++ engine/scene/scene.js
@@ -1,4 +1,4 @@
-import THREE from "../three.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 
 export const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x111111);
