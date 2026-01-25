60c5084f596bac3b2daa9197d6acf948ff0f6b0e
MyThekuruku <thekuruky@gmail.com>
2026-01-25 17:45:01 -0600

    Update controls.js

--- engine/controls/controls.js
+++ engine/controls/controls.js
@@ -1,16 +1,8 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { createCube } from "../objects/cube.js";
 
-export function createCube() {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({ color: 0x00ffcc });
-  const cube = new THREE.Mesh(geometry, material);
-
-  cube.position.set(
-    Math.random() * 2 - 1,
-    Math.random() * 2 - 1,
-    0
-  );
-
-  scene.add(cube);
-  console.log("🧊 Cubo creado");
+export function initControls() {
+  document.getElementById("createCube").onclick = () => {
+    createCube();
+  };
 }
+
