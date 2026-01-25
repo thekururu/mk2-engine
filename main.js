855a9912e734488253d1dc4bdf529c3ce1170cd0
MyThekuruku <thekuruky@gmail.com>
2026-01-25 16:45:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-// main.js
 
-// Escena
-import "./engine/scene/scene.js";
-
-// Controles
-import { initControls } from "./engine/controls/controls.js";
-
-// UI
-import "./ui/ui.js";
-
-// Inicializar controles
-initControls();
-
-console.log("🚀 Mini 3D Engine iniciado correctamente");
