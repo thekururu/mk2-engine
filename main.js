d92e76b6a2427c1433a96da0a499dd5121d86d3c
MyThekuruku <thekuruky@gmail.com>
2026-01-25 16:50:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+import "./engine/scene/scene.js";
+import { initControls } from "./engine/controls/controls.js";
+import "./ui/ui.js";
+
+initControls();
+console.log("🚀 Mini Engine limpio iniciado");
 
