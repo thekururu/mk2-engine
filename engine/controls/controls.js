5ffca050cc52f617390f099a334b2172bb9b03cf
MyThekuruku <thekuruky@gmail.com>
2026-01-25 16:48:01 -0600

    Update controls.js

--- engine/controls/controls.js
+++ engine/controls/controls.js
@@ -1,1 +1,27 @@
+import THREE from "../three.js";
+import { camera, scene, renderer } from "../scene/scene.js";
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+let selected = null;
+
+export function initControls() {
+  renderer.domElement.addEventListener("mousedown", e => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    const hits = raycaster.intersectObjects(scene.children);
+
+    if (hits.length) selected = hits[0].object;
+  });
+
+  renderer.domElement.addEventListener("mousemove", () => {
+    if (selected) selected.rotation.y += 0.03;
+  });
+
+  renderer.domElement.addEventListener("mouseup", () => {
+    selected = null;
+  });
+}
 
