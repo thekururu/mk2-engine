2f94014d5db590cdfe7a661b7e4a2626e0a88b48
MyThekuruku <thekuruky@gmail.com>
2026-01-25 17:19:42 -0600

    Update three.js

--- engine/three.js
+++ engine/three.js
@@ -1,3 +1,4 @@
+// engine/three.js
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 export default THREE;
-
+export * from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
